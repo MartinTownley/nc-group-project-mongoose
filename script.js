91c72d48b18cc2e105c0a61a2b3156c76c8cb794
FergusJames212 <fergusarjames@virginmedia.com>
2023-04-03 19:03:54 +0100

    json errors from chatgpt corrected for

--- script.js
+++ script.js
@@ -14,8 +14,9 @@ const generateLocations = async (inputString) => {
     model: "gpt-3.5-turbo",
     messages: [{ role: "user", content: inputString }],
   });
-  const contentFull = result.data.choices[0].message.content;
-  console.log(result.data.choices[0].message);
+  // console.log(result.data.choices[0].message, "<<uneddited");
+  // console.log(result.data.choices[0].message.content.replace(/`json|`/g,""), "<< fixed");
+  const contentFull = result.data.choices[0].message.content.replace(/`json|`/g,"");
   const contentFullParsed = JSON.parse(contentFull);
   const locations = Array.isArray(contentFullParsed)
     ? contentFullParsed
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import app from "./routes/trip-routes.js";
 console.log("");
 console.log("Server is starting please wait...");
 
-const PORT = process.env.PORT || 8989;
+const PORT = process.env.PORT || 8999;
 
 db.connect().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
